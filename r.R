d4d478cb41474f59ee906b0b1660d0620ed49ea4
LakshmiKrishnaGowda <137064689+LakshmiKrishnaGowda@users.noreply.github.com>
2023-06-20 17:27:37 +0530

    Add files via upload

---
+++ r.R
@@ -0,0 +1,7 @@
+citation();
+; close the statements
+: special symbols used in R function
+, special symbols used to separate elements
+"" used to assign elements 
+''used to assign elements
+# specifies a comment
